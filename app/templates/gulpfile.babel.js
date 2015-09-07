373cad728da91103ae564a034e6272cd77fcb0e8
Rakhat Jabagin <neilhem@hotmail.com>
2015-09-07 17:54:02 +0600

    update to autoprefixer 6.0

--- app/templates/gulpfile.babel.js
+++ app/templates/gulpfile.babel.js
@@ -17,7 +17,7 @@ gulp.task('styles', () => {
       precision: 10,
       includePaths: ['.']
     }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({browsers: ['last 1 version']}))
+    .pipe($.autoprefixer({browsers: ['> 1%', 'last 1 version', 'Firefox ESR']}))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('.tmp/styles'))
     .pipe(reload({stream: true}));
